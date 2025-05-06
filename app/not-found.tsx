374c377fcd6aac5b1b1e6b6d28410d89c2a59aa3
JulesFILIOT <dev@julesfiliot.com>
2025-05-06 18:01:47 +0200

    feat: change 404 page design

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,25 +1,18 @@
+import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
-      <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-6xl leading-9 font-extrabold tracking-tight text-gray-900 md:border-r-2 md:px-6 md:text-8xl md:leading-14 dark:text-gray-100">
-          404
-        </h1>
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <div className="flex items-center justify-center gap-4">
+        <h1 className="text-4xl tracking-[-0.84px]">404</h1>
+        <p className="text-sm">Page not found</p>
       </div>
-      <div className="max-w-md">
-        <p className="mb-4 text-xl leading-normal font-bold md:text-2xl">
-          Sorry we couldn't find this page.
-        </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
-        <Link
-          href="/"
-          className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm leading-5 font-medium text-white shadow-xs transition-colors duration-150 hover:bg-blue-700 focus:outline-hidden dark:hover:bg-blue-500"
-        >
-          Back to homepage
-        </Link>
-      </div>
+      <Link href="/" className="mt-6">
+        <Button className="w-fit cursor-pointer rounded-none bg-black px-6 py-4 text-white hover:bg-black/80 sm:mt-0">
+          <span className="text-sm leading-[16.8px] tracking-[-0.59px]">Back to home</span>
+        </Button>
+      </Link>
     </div>
   )
 }
